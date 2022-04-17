37c2f647ba4161777408432f162ef4bb369afde4
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-04-17 13:03:54 -0300

    Update pickCard.js

--- 01. Function Basics/pickCard.js
+++ 01. Function Basics/pickCard.js
@@ -3,6 +3,7 @@ The cards returned need to have a suit and a number.
 Numbers: 2345678910JQKA (14 numbers)
 Suits: Clubs, Spades, Hearts, Diamonds (4 suits)
 */
+
 let numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 let suits = ["Clubs", "Spades", "Diamonds", "Hearts"];
 
@@ -11,13 +12,10 @@ function arrayRand(array){
 }
 
 function drawCard(){
-    let numbRand = arrayRand(numbers);
-    let suitRand = arrayRand(suits);
-    let card = {
-        number: (numbers[numbRand]),
-        suit : suits[suitRand]
+    return {
+        number: (numbers[arrayRand(numbers)]),
+        suit : suits[arrayRand(suits)]
     }
-    return card
 }
 
 console.log(drawCard());
