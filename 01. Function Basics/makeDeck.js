e8d3b260b55d4887e81dd14a550c641a48faae39
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-04-20 10:19:13 -0300

    Update makeDeck.js

--- 01. Function Basics/makeDeck.js
+++ 01. Function Basics/makeDeck.js
@@ -1,36 +1,42 @@
-const myDeck = {
-    deck: [],
-    discardPile: [],
-    numbers: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ],
-    suits: ["Clubs", "Spades", "Diamonds", "Hearts"],
-    initializeDeck() {
-        const {suits, numbers, deck} = this;
-        for (let value of numbers){
-            for(let suit of suits){
-                deck.push({
-                    value,
-                    suit
-                })
+// Deck Factory Function that returns a deck object
+const makeDeck = () => {
+    return {
+        deck: [],
+        discardPile: [],
+        numbers: [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ],
+        suits: ["Clubs", "Spades", "Diamonds", "Hearts"],
+        initializeDeck() {
+            const {suits, numbers, deck} = this;
+            for (let value of numbers){
+                for(let suit of suits){
+                    deck.push({
+                        value,
+                        suit
+                    })
+                }
             }
-        }
-    },
-    drawCard() {
-        let drawnCard =  this.deck.pop();
-        this.discardPile.push(drawnCard);
-        return drawnCard;
-    },
-    drawMultiple(numCards) {
-        multipleDrawn = [];
-        for(let i = 0; i<numCards; i++){
-           multipleDrawn.push(this.drawCard());
-        }
-        return multipleDrawn;
-    },
-    suffleDeck(){
-        const {deck} = this;
-        for(let i = deck.length -1 ; i > 0; i--){
-            let j = Math.floor(Math.random() * (i+1));
-            [deck[i], deck[j]] = [deck[j], deck[i]];
+        },
+        drawCard() {
+            let drawnCard =  this.deck.pop();
+            this.discardPile.push(drawnCard);
+            return drawnCard;
+        },
+        drawMultiple(numCards) {
+            multipleDrawn = [];
+            for(let i = 0; i<numCards; i++){
+               multipleDrawn.push(this.drawCard());
+            }
+            return multipleDrawn;
+        },
+        suffleDeck(){
+            const {deck} = this;
+            for(let i = deck.length -1 ; i > 0; i--){
+                let j = Math.floor(Math.random() * (i+1));
+                [deck[i], deck[j]] = [deck[j], deck[i]];
+            }
         }
     }
 }
+
+const deck1 = makeDeck();
+const deck2 = makeDeck();
