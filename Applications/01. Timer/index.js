6ef31bf1b5967bb68e6e0c3f5a74c6db6954456f
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-04-24 13:45:55 -0300

    update timer

--- Applications/01. Timer/index.js
+++ Applications/01. Timer/index.js
@@ -1,4 +1,42 @@
 // The program will display a timer.
 // Its border will be revoluting (dissappearing) synchronously with the remaining time.
 
+class Timer{
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;    
+
+        this.startButton.addEventListener("click", this.start);
+        this.pauseButton.addEventListener("click", this.pause);
+    }
+
+    // Arrow functions are used in order to allow the class to call inner functions.
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000);
+    }
+  
+    pause = () => { 
+        clearInterval(this.interval);
+    }
+
+    tick = () => {
+        if (this.durationInput.value > 0) {
+            this.durationInput.value -= 1;
+        } else {
+            clearInterval(this.interval);
+            this.durationInput.type = "string";
+            this.durationInput.value = "Ding!";
+        }
+        
+    }; 
+
+}
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
 
