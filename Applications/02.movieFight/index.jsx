24c6167d7b72ccb93b9bd91bc56fba567e61c841
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-04-26 18:30:14 -0300

    movie-fight completed

--- Applications/02.movieFight/index.jsx
+++ Applications/02.movieFight/index.jsx
@@ -57,6 +57,7 @@ const onMovieSelected = async (movie, position) => {
         return []
     }
     document.querySelector(`#${position}-summary`).innerHTML = movieTemplate(movieResponse.data) 
+    
     if( position === "left"){
         leftMovie = movieResponse.data;
     }
@@ -70,15 +71,48 @@ const onMovieSelected = async (movie, position) => {
 }
 
 const runComparison = (rightMovie, leftMovie) => {
-    console.log(`Starting to compare ${rightMovie.Title} with ${leftMovie.Title}`)
+    const leftSideStats     = document.querySelectorAll("#left-summary .notification");
+    const rightSideStats    = document.querySelectorAll("#right-summary .notification");
+    
+    console.log(leftSideStats);
+    console.log(rightSideStats);
 
-    console.log(rightMovie)
-    
-    
+    leftSideStats.forEach((leftStat, index) => {
+        const rightStat = rightSideStats[index];
+
+        const leftSideValue = parseInt(leftStat.dataset.value);
+        const rightSideValue = parseInt(rightStat.dataset.value);
+        
+        if (rightSideValue > leftSideValue){
+            leftStat.classList.remove("is-primary")
+            leftStat.classList.add("is-warning");
+        } else {
+            rightStat.classList.remove("is-primary")
+            rightStat.classList.add("is-warning");
+        }   
+    });
 
 }
 
 const movieTemplate = (movieDetail) => {
+
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, ""))
+    const metascore = parseInt(movieDetail.Metascore)
+    const imdbRating = parseFloat(movieDetail.imdbRating)
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""))
+    
+    const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
+        const value = parseInt(word);
+        if(isNaN(value)) {
+            return prev;
+        } else{
+            return prev + value;
+        }
+    }, 0);
+
+    console.log(awards);
+
+
     return `
         <article class="media">
             <figure class="media-left">
@@ -95,23 +129,23 @@ const movieTemplate = (movieDetail) => {
             </div>
         </article>
 
-        <article class="notification is-primary ${movieDetail.Title.replace(/\s/g, '')}-awards">
+        <article data-value=${awards} class="notification is-primary ">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
-        <article class="notification is-primary ${movieDetail.Title.replace(/\s/g, '')}-box-office">
+        <article data-value=${dollars}  class="notification is-primary ">
             <p class="title">${movieDetail.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
-        <article class="notification is-primary ${movieDetail.Title.replace(/\s/g, '')}-metascore">
+        <article data-value=${metascore} class="notification is-primary ">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
-        <article class="notification is-primary ${movieDetail.Title.replace(/\s/g, '')}-imdb-rating">
+        <article data-value=${imdbRating} class="notification is-primary ">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>        
-        <article class="notification is-primary ${movieDetail.Title.replace(/\s/g, '')}-imdb-votes">
+        <article data-value=${imdbVotes} class="notification is-primary ">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
