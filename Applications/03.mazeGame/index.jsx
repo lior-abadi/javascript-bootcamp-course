a103cb972dee7bfa3acb7202c593a18179b0cb44
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-04-27 08:12:29 -0300

    update

--- Applications/03.mazeGame/index.jsx
+++ Applications/03.mazeGame/index.jsx
@@ -27,5 +27,16 @@ const walls = [
 ];
 World.add(world, walls)
 
-// Entities
+// Maze Generation
+
+const grid = []
+
+for (let i = 0; i < 3; i++) {
+   for (let j = 0; j < 3; j++) {
+       
+       
+   }
+    
+}
+
 
